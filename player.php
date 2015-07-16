8f1a81bf72b20a4819d8758be607a38609bdc81e
Brian McVeigh <bmcveigh@adgtech.net>
2015-07-16 16:25:59 -0400

    Commenting for functions and variables.

--- player.php
+++ player.php
@@ -6,26 +6,62 @@
  */
 
 class Player {
+  /**
+   * The player's full name.
+   */
   private $name;
+  
+  /**
+   * The set of cards that the player gets.
+   */
   private $hand;
 
+  /**
+   * Constructor for the Player class.
+   *
+   * @param string $name
+   *   The player's name.
+   *
+   */
   public function __construct($name) {
   	$this->name = $name;
   	$this->hand = array();
   }
 
+  /**
+   * Get the player's name.
+   *
+   * @return string
+   *   The player's name
+   */
   public function getName() {
   	return $this->name;
   }
 
+  /**
+   * Set the hand for the player.
+   *
+   * @param array $hand
+   *   An array of Hand objects.
+   */
   public function setHand($hand) {
   	$this->hand = $hand;
   }
 
+  /**
+   * Get the hand for a player object.
+   *
+   * @return
+   *   An array of Hand objects.
+   */
   public function getHand() {
   	return $this->hand;
   }
 
+  /**
+   * Play a card for the player. This will
+   * remove a card from the player's hand.
+   */
   public function playCard() {
 
   }
