9533e8c2099c5be7f5356c1e3f2fb192c294d0f5
Brian McVeigh <brianmcveigh@Brians-MacBook-Pro.local>
2015-07-18 14:56:54 -0400

    Cleaning up the code.

--- classes/deck.php
+++ classes/deck.php
@@ -4,54 +4,59 @@
  * @file
  */
 class Deck {
+  /**
+   * An array of 52 or 54 cards. A 54-card deck contains two Jokers,
+   * in addition to the 52 cards of a regular poker deck.
+   */
+  private $deck;
 
-  private $cards;
-  private $players;
-  private $numberOfPlayers;
+  /**
+   * Keeps track of the number of cards that have been dealt from
+   * the deck so far.
+   */
+  private $cardsUsed;
 
-  public function __construct($cards = NULL, $players = NULL) {
-    if (isset($cards) && isset($players)) {
-      $this->cards = $cards;
-      $this->players = $players;
-    }
-    else {
-      $players = array();
+  public function __construct($includeJokers = FALSE) {
+    $cardCount = 0;
 
-      $numberOfPlayers = 4;
-
-      for ($i = 0; $i < $numberOfPlayers; $i++) {
-        $players[] = new Player('Player ' . $i + 1);
+    for ($suit = 0; $suit <= 3; $suit++) {
+      for ($value = 1; $value <= 13; $value++) {
+        $this->deck[$cardCount] = new Card($value, $suit);
       }
     }
-  }
 
-  public function getCards() {
-    return $this->cards;
+    if ($includeJokers) {
+      $this->deck[] = new Card(1, Card::JOKER);
+      $this->deck[] = new Card(2, Card::JOKER);
+    }
+    $this->cardsUsed = 0;
   }
 
   /**
-   * Shuffles the array of cards. Ideally used
-   * right after creating a Deck object.
+   * Shuffles the array of cards into a random order.
    */
-  public function shuffleCards() {
-    shuffle($this->cards);
+  public function shuffleDeck() {
+    shuffle($this->deck);
   }
 
-  public function dealCards() {
-    $cardsPerPlayer = count($this->cards) / $this->numberOfPlayers;
-    $bookmark = 0;
+  /**
+   * As cards are dealt from the deck, the number of cards left
+   * decreases. This function returns the number of cards that
+   * are still left in the deck. The return value would be
+   * 52 or 54 (depending on whether the deck includes Jokers)
+   * when the deck is first created or after the deck has been
+   * shuffled. It decreases by 1 each time the dealCard() method
+   * is called.
+   */
+  public function cardsLeft() {
+    return count($this->deck) - $this->cardsUsed;
+  }
 
-    // Create a hand object with cards for each player.
-    for ($i = 0; $i < $this->numberOfPlayers; $i++) {
-      $items = array();
-
-      for ($j = $bookmark; $j < $cardsPerPlayer; $j++) {
-        $items[] = $this->cards[$j];
-        $bookmark = $j;
-      }
-
-      $hand = new Hand($items, $this->players[$i]->getName());
-      $this->players[$i]->setHand($hand);
+  public function dealCard() {
+    if ($this->cardsUsed === count($this->deck)) {
+      print 'No cards are left in the deck.\n';
     }
+
+    $this->cardsUsed++;
   }
 }
