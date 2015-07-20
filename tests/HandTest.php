92ff61d24606ea91455956777998dd07d9d42626
Brian McVeigh <brianmcveigh@Brians-MacBook-Pro.local>
2015-07-20 12:37:40 -0400

    Replaced tabs with two spaces.

--- tests/HandTest.php
+++ tests/HandTest.php
@@ -4,9 +4,9 @@ class HandTest extends PHPUnit_Framework_TestCase {
     public function testCanBeNegated() {
       $hand = new Hand();
 
-	  for ($i = 0; $i < 13; $i++) {
-	    $hand->addCard($deck->dealCard());
-	  }
-	  print $hand->getHandCount();
+    for ($i = 0; $i < 13; $i++) {
+      $hand->addCard($deck->dealCard());
+    }
+    print $hand->getHandCount();
     }
 }
